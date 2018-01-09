256c8bfb58f8398cd0e975238069f4c7dca9be33
martynka30 <32480411+martynka30@users.noreply.github.com>
2018-01-09 11:09:33 +0100

    Add files via upload

---
+++ Poland - Block 10 Interactive Website using JS - Part 2/script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    $("#slider").hover(function(){
+      $(this).animate({left:"0px"}); 
+    },
+   function(){
+        $(this).animate({left:"-292px"});
+    }
+    );
+};
